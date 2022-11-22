ae901124c41f287d2333b43d0a7492f902b1b52c
haasova.ka.2019 <haasova.ka.2019@UC313PC05>
2022-11-22 15:21:08 +0100

    Přidejte soubory projektu.

---
+++ 221122 Cesarova šifra/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _221122_Cesarova_šifra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Encrypt("ahoj maki", 12);
+            Console.WriteLine(s);
+            Console.WriteLine(Decrypt(s, 12));
+            Console.ReadLine();
+        }
+
+        static string Encrypt(string message, int key)
+        {
+            string encryptedMessage = "";
+            int i = 0;
+            foreach (char item in message)
+            {
+                if (!(item < 97 || item > 122))
+                {
+                    i = item + key;
+                    if (i > 122)
+                        i -= 26;
+                    encryptedMessage += (char)i;
+                }
+            }
+
+            return encryptedMessage;
+        }
+
+        static string Decrypt(string message, int key)
+        {
+            string decryptedMessage = "";
+            int i = 0;
+            foreach (char item in message)
+            {
+                if (!(item < 97 || item > 122))
+                {
+                    i = item - key;
+                    if (i < 97)
+                        i += 26;
+                    decryptedMessage += (char)i;
+                }
+            }
+
+            return decryptedMessage;
+        }
+    }
+}
